10142d485d8f22409b9c46d76147847265e6f82a
Jean <jean-marco-10@hotmail.com>
2018-08-25 15:24:08 -0600

    Articles CRUD functionality ready

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,18 @@
 class PostsController < ApplicationController
     def index
         @posts = Post.all
-    end
+      end
 
     def show
         @post = Post.find(params[:id])
     end
-    
+
     def new
         @post = Post.new
+    end
+
+    def edit
+        @post = Post.find(params[:id])
     end
 
     def create
@@ -17,12 +21,30 @@ class PostsController < ApplicationController
         if @post.save
             redirect_to @post
         else
-            render 'new'
+            #render json: @post.errors.messages
+            render :new
         end
     end
 
-    private
-        def post_params
-            params.require(:post).permit(:title, :description)
+    def update
+        @post = Post.find(params[:id])
+       
+        if @post.update(post_params)
+          redirect_to @post
+        else
+          render :edit
         end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+       
+        redirect_to posts_path
+      end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :description)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
     validates :title, presence: true,
-                    length: { minimum: 5 }
+                    length: { minimum: 3 }
     validates :description, length: { minimum: 5 }
 end
