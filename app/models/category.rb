63d7c9900354ba3f71242666bad10c74df9fa238
Jean <jean-marco-10@hotmail.com>
2018-08-30 13:33:24 -0600

    Categories association with articles many to many

---
+++ app/models/category.rb
@@ -0,0 +1,5 @@
+class Category < ApplicationRecord
+  has_many :has_categories, dependent: :destroy
+  has_many :posts, through: :has_categories
+  accepts_nested_attributes_for :has_categories
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :has_categories, dependent: :destroy
+  has_many :categories, through: :has_categories
 
-    validates :title, presence: true,
-                    length: { minimum: 3 }
-    validates :description, length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :description, length: { minimum: 5 }
 end
