2a26a13f83c26d00bde7cf84469b8b921d230cd2
Jean <jean-marco-10@hotmail.com>
2018-08-30 19:33:45 -0600

    Admin and editor roles, with authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate_editor!
+    redirect_to root_path, :flash => { :error => "Not allowed!" } unless user_signed_in? && current_user.editor?
+  end
+
+  def authenticate_admin!
+    redirect_to root_path, :flash => { :error => "Not allowed!" } unless user_signed_in? && current_user.admin?
+  end
 end
