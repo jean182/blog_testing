ec4ba92ff3795abc1423fd818640728d345aba4e
Jean <jean-marco-10@hotmail.com>
2018-09-03 10:55:00 -0600

    Added username field and modify the login to being able to accept usernames and emails as well

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,13 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:name, :email, :password, :password_confirmation)
+      user_params.permit(:username, :name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:username, :email, :name, :password, :password_confirmation, :current_password)
     end
   end
 
