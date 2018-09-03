9cad77943ca4642096e213887e905e5821c8c7e2
Jean <jean-marco-10@hotmail.com>
2018-09-03 10:15:10 -0600

    Added name field into sign up view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
 
   def authenticate_editor!
     redirect_to posts_path, :flash => { :error => "You don't have the permissions to do this!" } unless user_signed_in? && current_user.editor?
